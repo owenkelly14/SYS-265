3642206e943d90d7b4209c828227e5563d81ca5c
owenkelly14 <owen.kelly@mymail.champlain.edu>
2026-02-16 15:35:22 -0500

    Added secure-ssh.sh

---
+++ linux/centos7/secure-ssh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# secure-ssh.sh
+# createsa a new SSH user using $1 parameter
+# adds a public key from the local repo
+# removes root ability to SSH in
+
+
+echo "ALL YOUR CODE GOES HERE"
