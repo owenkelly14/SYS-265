acfca586f4730ff9b6b03256aa8fd2ed6171ecc3
owenkelly14 <owen.kelly@mymail.champlain.edu>
2026-02-16 22:56:02 +0000

    completed script

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
 # secure-ssh.sh
-# createsa a new SSH user using $1 parameter
+# creates a new SSH user using $1 parameter
 # adds a public key from the local repo
 # removes root ability to SSH in
 
 
-echo "ALL YOUR CODE GOES HERE"
+sudo useradd -m -d /home/${1} -s /bin/bash ${1}
+sudo mkdir /home/${1}/.ssh
+sudo cp /home/owen/SYS-265/linux/public-keys/id_rsa.pub /home/${1}/.ssh/authorized_keys
+sudo chmod 700 /home/${1}/.ssh
+sudo chmod 600 /home/${1}/.ssh/authorized_keys
+sudo chown -R ${1}:${1} /home/${1}/.ssh
+
+sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin no /' /etc/ssh/sshd_config
+
+sudo systemctl restart ssh
+
